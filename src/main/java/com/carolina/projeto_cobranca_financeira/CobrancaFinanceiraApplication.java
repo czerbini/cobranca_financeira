11fbecff4d0811468a0dcc6341fec6c7dbe254bd
carolina.azerbini <cmzerbini@hotmail.com>
2026-03-29 13:18:59 -0300

    feat: initial project setup

---
+++ src/main/java/com/carolina/projeto_cobranca_financeira/CobrancaFinanceiraApplication.java
@@ -0,0 +1,13 @@
+package com.carolina.projeto_cobranca_financeira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobrancaFinanceiraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CobrancaFinanceiraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carolina/projeto_cobranca_financeira/CobrancaFinanceiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carolina.projeto_cobranca_financeira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CobrancaFinanceiraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
